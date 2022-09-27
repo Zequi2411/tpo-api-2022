8e7d5c6a574b5b32079b373a0d58434686b1f6ed
zequi2411 <ezequielnf2411@gmail.com>
2022-09-27 19:29:55 -0300

    Intento Search

--- src/components/paginas/ItemListContainer.js
+++ src/components/paginas/ItemListContainer.js
@@ -5,6 +5,7 @@ import '../../App.css';
 
 const ItemListConteiner = ({ greeting }) => {
   const [productList, setProductList] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getProducts.then((response) => {
@@ -22,6 +23,19 @@ const ItemListConteiner = ({ greeting }) => {
   return (
     <div className="divPadreItemListConteiner">
       <h1> {greeting} </h1>
+
+      <table className='table table-striped table-hover mt-5 shadow-lg'>
+        <thead>
+          <tr className='bg-curso text-white'>
+            <th>NOMBRE</th>
+            <th>MATERIA</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+
       <ItemList lista={productList}/>
     </div>
   )
