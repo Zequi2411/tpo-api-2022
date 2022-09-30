a0ed2a4c0a7516240a48a5ec178da1fc416d6875
Gabirojas1 <gabriel.a.rojas.ar@gmail.com>
2022-09-30 18:36:00 -0300

    update

--- src/components/Solicitudes/Solicitudes.js
+++ src/components/Solicitudes/Solicitudes.js
@@ -1,15 +1,7 @@
-import Solicitud from "./Solicitud"
 
-const Solicitudes = ({ tasks, onDelete, onToggle }) => {
-  return (
-  
-    <>
-     {tasks.map((task) => (
-        <Solicitud key={task.id} task={task} 
-        onDelete={onDelete} onToggle={onToggle} />
-     ))} 
-    </>
-  )
+
+const Solicitudes = () => {
+
 }
   
 
--- src/components/paginas/PaginaSolicitud.js
+++ src/components/paginas/PaginaSolicitud.js
@@ -4,48 +4,6 @@ import Solicitudes from "../Solicitudes/Solicitudes"
 
 function paginaSolicitud() {
 
-    const[tasks, setTasks] = useState([
-        {
-          id: 1,
-          text: 'Doctors Appointment',
-          day: '5 de febrero 2:30 pm',
-          reminder: true,
-      },
-      {
-          id: 2,
-          text: 'Meeting at School',
-          day: '6 de febrero 1:30 pm',
-          reminder: true,
-      },
-      {
-          id: 3,
-          text: 'Food Shopping',
-          day: '5 de febrero 2:30 pm',
-          reminder: false,
-      },
-      ])
-
-      const deleteTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-      }
-    
-    //toggle Reminder
-    
-    const toggleReminder = (id) => {
-      setTasks(tasks.map((task) => task.id === id ?
-      {...task, reminder: !task.reminder} : task))
-    }
-
-    return(
-        <div>
-            {tasks.length > 0 ? (
-                <Solicitudes tasks={tasks} onDelete={deleteTask} 
-                onToggle={toggleReminder} />
-            ) : (
-                'No Tasks To Show'
-            )}
-        </div>
-    )
 }
 
 export default paginaSolicitud
